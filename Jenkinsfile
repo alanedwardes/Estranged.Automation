54a48f3a36c61c6bc97f46bfe7cd75a9fc33a00b
Alan Edwardes <alan@alanedwardes.com>
2018-04-20 22:29:00 +0100

    Adds discord runner project.

--- Jenkinsfile
+++ Jenkinsfile
@@ -19,6 +19,7 @@ node("linux") {
 				string(credentialsId: 'SlackWebHookUrl', variable: 'SLACK_WEB_HOOK_URL'),
 				string(credentialsId: 'SyndicationWebHookUrl', variable: 'SYNDICATION_WEB_HOOK_URL'),
 				string(credentialsId: 'ReviewsWebHookUrl', variable: 'REVIEWS_WEB_HOOK_URL'),
+				string(credentialsId: 'DiscordBotToken', variable: 'DISCORD_BOT_TOKEN'),
 				file(credentialsId: 'GoogleComputeEstrangedAutomation', variable: 'GOOGLE_APPLICATION_CREDENTIALS'),
 				[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'JenkinsEstrangedAutomation']
 			]) {
---
+++ src/Runner.Discord/DiscordRunner.cs
@@ -0,0 +1,67 @@
+﻿using Estranged.Automation.Shared;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Discord.WebSocket;
+using Discord;
+using System;
+
+namespace Estranged.Automation.Runner.Syndication
+{
+    public class DiscordRunner : IRunner
+    {
+        private readonly ILogger<DiscordRunner> logger;
+
+        public DiscordRunner(ILogger<DiscordRunner> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task Run(CancellationToken token)
+        {
+            var client = new DiscordSocketClient();
+
+            client.Log += ClientLog;
+
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN"));
+            await client.StartAsync();
+
+            client.MessageReceived += ClientMessageReceived;
+        }
+
+        private async Task ClientMessageReceived(SocketMessage socketMessage)
+        {
+            if (socketMessage.Content != null && socketMessage.Content.ToLower().Contains("linux"))
+            {
+                await socketMessage.Channel.SendMessageAsync("I'd just like to interject for a moment. What you’re referring to as Linux, is in fact, GNU/Linux, or as I’ve recently taken to calling it, GNU plus Linux.");
+            }
+        }
+
+        private Task ClientLog(LogMessage logMessage)
+        {
+            logger.Log(GetLogLevel(logMessage.Severity), -1, logMessage.Message, logMessage.Exception, null);
+            return null;
+        }
+
+        private LogLevel GetLogLevel(LogSeverity severity)
+        {
+            switch (severity)
+            {
+                case LogSeverity.Critical:
+                    return LogLevel.Critical;
+                case LogSeverity.Debug:
+                    return LogLevel.Debug;
+                case LogSeverity.Error:
+                    return LogLevel.Error;
+                case LogSeverity.Info:
+                    return LogLevel.Information;
+                case LogSeverity.Verbose:
+                    return LogLevel.Trace;
+                case LogSeverity.Warning:
+                    return LogLevel.Warning;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
