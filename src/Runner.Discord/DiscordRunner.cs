723cc44c8c0f066153a3fc411e5cb5c88a66ac8e
Alan Edwardes <alan@alanedwardes.com>
2018-04-22 15:30:11 +0100

    Wait for task completion.

--- src/Runner.Discord/DiscordRunner.cs
+++ src/Runner.Discord/DiscordRunner.cs
@@ -47,17 +47,14 @@ namespace Estranged.Automation.Runner.Syndication
             await Task.Delay(-1, token);
         }
 
-        private Task ClientMessageReceived(IServiceProvider provider, SocketMessage socketMessage, CancellationToken token)
+        private async Task ClientMessageReceived(IServiceProvider provider, SocketMessage socketMessage, CancellationToken token)
         {
             if (socketMessage.Author.IsBot || socketMessage.Author.IsWebhook || string.IsNullOrWhiteSpace(socketMessage.Content))
             {
-                return Task.CompletedTask;
+                return;
             }
 
-            // Run all message processors in parallel, don't wait for completion
-            Task.WhenAll(provider.GetServices<IResponder>().Select(x => x.ProcessMessage(socketMessage, token)));
-
-            return Task.CompletedTask;
+            await Task.WhenAll(provider.GetServices<IResponder>().Select(x => x.ProcessMessage(socketMessage, token)));
         }
 
         private Task ClientLog(LogMessage logMessage)
