03954c518c83b0a2efa543b5518436e34bd1c7fc
Alan Edwardes <alan@alanedwardes.com>
2018-05-06 18:36:49 +0100

    Adds breed detector.

--- src/Runner.Discord/Responders/DogResponder.cs
+++ src/Runner.Discord/Responders/DogResponder.cs
@@ -15,6 +15,8 @@ namespace Estranged.Automation.Runner.Discord.Responders
 
         private string[] breeds = new[] { "shiba", "corgi", "samoyed" };
 
+        private string[] allBreeds = new[] { "affenpinscher", "african", "airedale", "akita", "appenzeller", "basenji", "beagle", "bluetick", "borzoi", "bouvier", "boxer", "brabancon", "briard", "bulldog", "bullterrier", "cairn", "chihuahua", "chow", "clumber", "collie", "coonhound", "corgi", "dachshund", "dalmatian", "dane", "deerhound", "dhole", "dingo", "doberman", "elkhound", "entlebucher", "eskimo", "germanshepherd", "greyhound", "groenendael", "hound", "husky", "keeshond", "kelpie", "komondor", "kuvasz", "labrador", "leonberg", "lhasa", "malamute", "malinois", "maltese", "mastiff", "mexicanhairless", "mix", "mountain", "newfoundland", "otterhound", "papillon", "pekinese", "pembroke", "pinscher", "pointer", "pomeranian", "poodle", "pug", "pyrenees", "redbone", "retriever", "ridgeback", "rottweiler", "saluki", "samoyed", "schipperke", "schnauzer", "setter", "sheepdog", "shiba", "shihtzu", "spaniel", "springer", "stbernard", "terrier", "vizsla", "weimaraner", "whippet", "wolfhound" };
+
         public DogResponder(HttpClient httpClient)
         {
             this.httpClient = httpClient;
@@ -28,12 +30,23 @@ namespace Estranged.Automation.Runner.Discord.Responders
                 return;
             }
 
-            using (message.Channel.EnterTypingState(token.ToRequestOptions()))
+            var intersections = words.Intersect(allBreeds).ToArray();
+            if (intersections.Any())
             {
-                var breed = breeds.OrderBy(x => Guid.NewGuid()).First();
+                await SendPhoto(intersections.First(), message.Channel, token);
+                return;
+            }
 
+            // Send random breed
+            await SendPhoto(breeds.OrderBy(x => Guid.NewGuid()).First(), message.Channel, token);
+        }
+
+        private async Task SendPhoto(string breed, IMessageChannel channel, CancellationToken token)
+        {
+            using (channel.EnterTypingState(token.ToRequestOptions()))
+            {
                 var dog = JObject.Parse(await httpClient.GetStringAsync($"https://dog.ceo/api/breed/{breed}/images/random"));
-                await message.Channel.SendMessageAsync(dog.Value<string>("message"));
+                await channel.SendMessageAsync(dog.Value<string>("message"));
             }
         }
     }
