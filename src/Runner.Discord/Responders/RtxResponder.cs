91dac26e128d21999f2bf996b6d38c66dadc13a2
Alan Edwardes <alan@alanedwardes.com>
2018-09-16 16:33:37 +0100

    Adds RtxResponder.

---
+++ src/Runner.Discord/Responders/RtxResponder.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Estranged.Automation.Runner.Discord.Responders
+{
+    public class RtxResponder : IResponder
+    {
+        public async Task ProcessMessage(IMessage message, CancellationToken token)
+        {
+            var trimmed = message.Content.ToLower().Trim();
+            if (trimmed.Contains("rtx on") || trimmed.Contains("rtx off"))
+            {
+                await message.Channel.SendMessageAsync("AND JUST LIKE THAT", options: token.ToRequestOptions());
+            }
+        }
+    }
+}
